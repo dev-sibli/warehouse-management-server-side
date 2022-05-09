cc458c651b0d41c4022835fcce28905eca85b691
dev-sibli <siblisadik13@gmail.com>
2022-05-09 20:48:52 +0600

    data loaded

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t6q9s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const tvCollection = client.db('tvStar').collection('tv');
+
+        //TV API
+        app.get('/tv', async (req, res) => {
+            const query = {};
+            const cursor = tvCollection.find(query)
+            const tvStar = await cursor.toArray();
+            res.send(tvStar);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Tv star Server running')
 })
